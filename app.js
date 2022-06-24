0db3813729247e7ff6b0bfab312ce6471179eed0
yang <joey674901205@gmail.com>
2022-06-24 15:34:14 -0400

    generatepage by node module, and create a file can be exported

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    // this...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('==================');
+// console.log(profileDataArgs);
 
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// const printProfileData = profileDataArr => {
+//     // this...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('==================');
+//     // is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+// printProfileData(profileDataArgs);
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
