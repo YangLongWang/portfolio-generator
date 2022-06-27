b88b74643fa0fa07179645f2108dc19b09b44863
yang <joey674901205@gmail.com>
2022-06-27 15:51:19 -0400

    follow online guiding

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-// console.log(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-// const printProfileData = profileDataArr => {
-//     // this...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('==================');
-//     // is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-// printProfileData(profileDataArgs);
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
